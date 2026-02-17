8d59823566100168626840214351cb75e9aa5fd0
vifrus <vifru.s@gmail.com>
2026-02-17 13:40:38 -0300

    Fix love.js version mismatch: change t.version from 11.5 to 11.4 (#6)

--- games/dialog_basic/conf.lua
+++ games/dialog_basic/conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.title = "Dialog Base"
-    t.version = "11.5"
+    t.version = "11.4"
     t.window.width = 800
     t.window.height = 600
     t.window.resizable = true
