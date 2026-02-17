0763d5ec3fe6376f5d113583334275eade75b406
vifrus <vifru.s@gmail.com>
2026-02-17 12:04:41 -0300

    Make dialog_basic game responsive for mobile screens (#5)

--- games/dialog_basic/conf.lua
+++ games/dialog_basic/conf.lua
@@ -4,6 +4,8 @@ function love.conf(t)
     t.window.width = 800
     t.window.height = 600
     t.window.resizable = true
+    t.window.minwidth = 320
+    t.window.minheight = 240
     t.modules.joystick = false
     t.modules.physics = false
 end
